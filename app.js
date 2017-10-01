4386e2a4ca8792dc16be0648d86a3e83817a4349
Siu Soan <siusoandesign@gmail.com>
2017-10-01 12:26:37 +0200

    adding new route/view for about pages

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 
 app.set('postsFile', require('./config/posts.json'));
 app.set('tagsFile', require('./config/tags.json'));
+app.set('aboutFile', require('./config/about.json'));
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -13,6 +14,7 @@ app.use(express.static('public'))
 app.use(require("./routes/root_router"));
 app.use(require("./routes/posts_router"));
 app.use(require("./routes/tags_router"));
+app.use(require("./routes/about_router"));
 
 app.listen(app.get('port'), function() {
   console.log('Node luistert op poort', app.get('port'));
